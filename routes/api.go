67f34713bf5e0f7c760b26d78fa6eb3874633b97
skript023 <fatur_muhammad@bk.ru>
2022-12-19 00:48:26 +0700

    Added more routes

--- routes/api.go
+++ routes/api.go
@@ -7,10 +7,17 @@ import (
 )
 
 func SetupRoute(app *fiber.App) {
-	app.Get("/api", func(res *fiber.Ctx) error {
+	api := app.Group("/api/v1")
+	auth := api.Group("/auth")
+	user := api.Group("/user")
+
+	api.Get("/welcome", func(res *fiber.Ctx) error {
 		return res.SendString("Welcome to my API")
 	})
 
-	app.Post("/api/v1/user/create", controller.CreateUser)
-	app.Delete("/api/v1/user/delete/:id", controller.DeleteUser)
+	auth.Post("/login", controller.Login)
+
+	user.Post("/create", controller.CreateUser)
+	user.Patch("/update/:id", controller.UpdateUser)
+	user.Delete("/delete/:id", controller.DeleteUser)
 }
